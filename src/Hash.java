3feba5154ce3f06c298f5880f593d021a1a02ed8
AgentP217 <agentp217@gmail.com>
2016-09-16 23:40:44 +0700

    Added Hash function

--- src/Hash.java
+++ src/Hash.java
@@ -1,5 +1,26 @@
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 public class Hash
 {
-    
+    public String hash(String pass) throws NoSuchAlgorithmException
+    {
+        MessageDigest messageDigest = null;
+        byte [] digest = new byte[0];
+
+        messageDigest = MessageDigest.getInstance("MD5");
+        messageDigest.reset();
+        messageDigest.update(pass.getBytes());
+        digest=messageDigest.digest();
+
+        BigInteger bigInt = new BigInteger(1,digest);
+        String md5Hex = bigInt.toString(16);
+
+        while (md5Hex.length()<32)
+        {
+            md5Hex="0"+md5Hex;
+        }
+        return md5Hex;
+    }
 }
