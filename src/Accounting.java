66f0ec037220d4005f9a20ee3522709b43eaa2b1
RuiFoxx <fox.uchiha95@gmail.com>
2016-09-17 07:17:43 +0700

    Added java classes for role and accounting

---
+++ src/Accounting.java
@@ -0,0 +1,13 @@
+public class Accounting {
+    private int id, volume;
+    private Role role;
+    private String date_start, date_end;
+
+    public Accounting(int id, int volume, Role role, String date_start, String date_end) {
+        this.id = id;
+        this.volume = volume;
+        this.role = role;
+        this.date_start = date_start;
+        this.date_end = date_end;
+    }
+}
---
+++ src/Role.java
@@ -0,0 +1,12 @@
+public class Role {
+    private int id;
+    private User user;
+    private String name, resource;
+
+    public Role(int id, User user, String name, String resource) {
+        this.id = id;
+        this.user = user;
+        this.name = name;
+        this.resource = resource;
+    }
+}
