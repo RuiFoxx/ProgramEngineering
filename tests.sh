4f29677871549a8cb13178c6bf7164e2d70b422d
RuiFoxx <fox.uchiha95@gmail.com>
2016-10-20 11:14:20 +0700

    Optimized tests.sh

--- tests.sh
+++ tests.sh
@@ -1,14 +1,39 @@
-java -cp "./lib/commons-cli-1.3.1.jar;./out/production/ProgramEngineering" com.Main ; echo -e "Exit code: " $? "\n"
-java -cp "./lib/commons-cli-1.3.1.jar;./out/production/ProgramEngineering" com.Main  -h ; echo -e "Exit code: " $? "\n"
-java -cp "./lib/commons-cli-1.3.1.jar;./out/production/ProgramEngineering" com.Main  -login 'XXX' -pass 'XXX' ; echo -e "Exit code: " $? "\n"
-java -cp "./lib/commons-cli-1.3.1.jar;./out/production/ProgramEngineering" com.Main  -login 'jdoe' -pass 'XXX' ; echo -e "Exit code: " $? "\n"
-java -cp "./lib/commons-cli-1.3.1.jar;./out/production/ProgramEngineering" com.Main  -login 'jdoe' -pass 'sup3rpaZZ' ; echo -e "Exit code: " $? "\n"
-java -cp "./lib/commons-cli-1.3.1.jar;./out/production/ProgramEngineering" com.Main  -login 'jdoe' -pass 'sup3rpaZZ' -role 'READ' -res 'a' ; echo -e "Exit code: " $? "\n"
-java -cp "./lib/commons-cli-1.3.1.jar;./out/production/ProgramEngineering" com.Main  -login 'jdoe' -pass 'sup3rpaZZ' -role 'READ' -res 'a.b' ; echo -e "Exit code: " $? "\n"
-java -cp "./lib/commons-cli-1.3.1.jar;./out/production/ProgramEngineering" com.Main  -login 'jdoe' -pass 'sup3rpaZZ' -role 'XXX' -res 'a.b' ; echo -e "Exit code: " $? "\n"
-java -cp "./lib/commons-cli-1.3.1.jar;./out/production/ProgramEngineering" com.Main  -login 'jdoe' -pass 'sup3rpaZZ' -role 'READ' -res 'XXX' ; echo -e "Exit code: " $? "\n"
-java -cp "./lib/commons-cli-1.3.1.jar;./out/production/ProgramEngineering" com.Main  -login 'jdoe' -pass 'sup3rpaZZ' -role 'WRITE' -res 'a' ; echo -e "Exit code: " $? "\n"
-java -cp "./lib/commons-cli-1.3.1.jar;./out/production/ProgramEngineering" com.Main  -login 'jdoe' -pass 'sup3rpaZZ' -role 'WRITE' -res 'a.bc' ; echo -e "Exit code: " $? "\n"
-java -cp "./lib/commons-cli-1.3.1.jar;./out/production/ProgramEngineering" com.Main  -login 'jdoe' -pass 'sup3rpaZZ' -role 'READ' -res 'a.b' -ds '2015-01-01' -de '2015-12-31' -vol '100' ; echo -e "Exit code: " $? "\n"
-java -cp "./lib/commons-cli-1.3.1.jar;./out/production/ProgramEngineering" com.Main  -login 'jdoe' -pass 'sup3rpaZZ' -role 'READ' -res 'a.b' -ds '01-01-2015' -de '2015-12-31' -vol '100' ; echo -e "Exit code: " $? "\n"
-java -cp "./lib/commons-cli-1.3.1.jar;./out/production/ProgramEngineering" com.Main  -login 'jdoe' -pass 'sup3rpaZZ' -role 'READ' -res 'a.b' -ds '2015-01-01' -de '2015-12-31' -vol 'XXX' ; echo -e "Exit code: " $? "\n"
+result=0 # number of failed tests
+
+test () {
+    arr=($1)
+    ./run.sh ${arr[*]}
+    status=$?
+    if [[ $status -ne $2 ]]; then
+        echo TESTING FAIL [$1] $status "!=" $2
+        ((result+=1))
+    else
+        echo TESTING OK [$1] $status "==" $2
+    fi
+    return $status
+}
+
+./build.sh
+
+test "" 0
+test "-h" 0
+test "-l XXX -p XXX" 1
+test "-l jdoe -p XXX" 2
+test "-l jdoe -p sup3rpaZZ" 0
+test "-l jdoe -p sup3rpaZZ -rol READ -res a" 0
+test "-l jdoe -p sup3rpaZZ -rol READ -res a.b" 0
+test "-l jdoe -p sup3rpaZZ -rol XXX -res a.b" 3
+test "-l jdoe -p sup3rpaZZ -rol READ -res XXX" 4
+test "-l jdoe -p sup3rpaZZ -rol WRITE -res a" 4
+test "-l jdoe -p sup3rpaZZ -rol WRITE -res a.bc" 4
+test "-l jdoe -p sup3rpaZZ -rol READ -res a.b -ds 2015-01-01 -de 2015-12-31 -vol 100" 0
+test "-l jdoe -p sup3rpaZZ -rol READ -res a.b -ds 01-01-2015 -de 2015-12-31 -vol 100" 5
+test "-l jdoe -p sup3rpaZZ -rol READ -res a.b -ds 2015-01-01 -de 2015-12-31 -vol XXX" 5
+
+echo
+if [[ $result -gt 0 ]]; then
+    echo $result tests failed
+else
+    echo ALL TESTS PASSED
+fi
+exit ${result}
