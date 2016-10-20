a72cbbd5b389351ed5698d91e3d7060b57d8fcc3
RuiFoxx <fox.uchiha95@gmail.com>
2016-10-20 11:52:47 +0700

    Release 2

--- build.sh
+++ build.sh
@@ -1,17 +1,16 @@
 source ./config.sh
 
 rm -rf $OUT
-echo "Компиляция всех *.java файлов"
+#echo "Компиляция всех *.java файлов"
 mkdir -p $OUT_CLS
 find . -name "*.java" | xargs javac -cp "$CP" -d $OUT_CLS -sourcepath $SRC #-verbose
 
-# Копирование их из $RES в $OUT_CLS
+# echo "Копирование их из $RES в $OUT_CLS"
 #cp -r &RES $OUT_CLS
 
-echo "Копирование библиотек"
+#echo "Копирование библиотек"
 mkdir -p $OUT_LIB
 cp $LIB $OUT_LIB
 
-echo "Архивация всех классов в $OUT_JAR в jar файл, где $MAIN – класс с main()"
+#echo "Архивация всех классов в $OUT_JAR в jar файл, где $MAIN – класс с main()"
 jar -cfe $OUT_JAR $MAIN -C $OUT_CLS .
-#javac -sourcepath "./src" -classpath "./lib/commons-cli-1.3.1.jar;" src/com/Main.java -d out/
