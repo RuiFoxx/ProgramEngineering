f3e72acf12baf1b0cb9e262373b7d72a83eb6333
RuiFoxx <fox.uchiha95@gmail.com>
2016-11-25 22:47:15 +0700

    (+) Testing parsing function

--- src/test/java/TestCli.java
+++ src/test/java/TestCli.java
@@ -1,19 +1,20 @@
-import com.*;
+import com.AaaDao;
+import com.Cli;
 import com.domain.Role;
 import com.domain.User;
-import org.flywaydb.core.Flyway;
-import org.junit.*;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
 import java.util.ArrayList;
 
-import static org.junit.Assert.*;
-import static org.mockito.Mockito.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class TestCli {
     private static AaaDao aaa;
-    private static Connection conn;
     private static User curUser;
     private static ArrayList<Role> roles = new ArrayList<>();
 
@@ -108,5 +109,24 @@ public class TestCli {
         when(aaa.getRoles(curUser.getId())).thenReturn(roles);
         assertEquals(5, new Cli().parse(aaa, "-l", "jdoe", "-p", "sup3rpaZZ", "-rol", "READ", "-res", "a",
                 "-ds", "2061-01-20", "-de", "206-01-21", "-vol", "-5"));
+    }
+
+    @Test
+    public void testArgs() throws Throwable {
+        when(aaa.getUsers("jdoe")).thenReturn(curUser = new User(1, "John Doe", "jdoe", "sup3rpaZZ"));
+        roles.add(new Role(1, curUser.getId(), "READ", "a"));
+        roles.add(new Role(2, curUser.getId(), "WRITE", "a.b"));
+        roles.add(new Role(4, curUser.getId(), "EXECUTE", "a.b.c"));
+        when(aaa.getRoles(curUser.getId())).thenReturn(roles);
+
+        new Cli().parse(aaa, "-p", "sup3rpaZZ", "-l", "jdoe", "-rol", "READ", "-res", "a",
+                "-ds", "2016-02-02", "-de", "2016-03-03", "-vol", "20");
+        assertEquals(Cli.getCmdData().getLogin(), "jdoe");
+        assertEquals(Cli.getCmdData().getPassword(), "sup3rpaZZ");
+        assertEquals(Cli.getCmdData().getResource(), "a");
+        assertEquals(Cli.getCmdData().getRole(), "READ");
+        assertEquals(Cli.getCmdData().getDateStart(), "2016-02-02");
+        assertEquals(Cli.getCmdData().getDateEnd(), "2016-03-03");
+        assertEquals(Cli.getCmdData().getVolume(), "20");
     }
 }
