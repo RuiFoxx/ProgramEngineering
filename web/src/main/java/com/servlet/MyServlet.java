f4f8097af01e466ec1aa28a8b828e4d678f822c4
AgentP217 <agentp217@gmail.com>
2016-12-04 18:06:58 +0700

    Release 5.0

---
+++ web/src/main/java/com/servlet/MyServlet.java
@@ -0,0 +1,23 @@
+package com.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Arrays;
+
+public class MyServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+            resp.getWriter().println("ID = " + Arrays.toString(req.getParameterValues("id")));
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        if(req.getRequestURI().equals("/echo/post")) {
+            doGet(req, resp);
+        }
+        else resp.setStatus(404);
+    }
+}
---
+++ web/src/main/webapp/index.jsp
@@ -0,0 +1,9 @@
+<html>
+<body>
+<h2>Application for AAA Protocol</h2>
+<form action = "/echo/post" method = "post">
+  ID: <input type="text" name="id"><br><br>
+  <input type="submit" value="Submit">
+</form>
+</body>
+</html>
